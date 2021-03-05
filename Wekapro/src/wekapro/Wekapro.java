374ef5e39bab34c77b67a2249f3618684ddade9b
tuyetnhi220 <trantuyetnhi22032000@gmail.com>
2021-03-05 18:06:36 +0700

    NHi

--- Wekapro/src/wekapro/Wekapro.java
+++ Wekapro/src/wekapro/Wekapro.java
@@ -28,11 +28,18 @@ public class Wekapro {
 //             "-N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1 -S -1.0 -c -1",
 //             "-R 2-3");
 //     model.mineAssociationaRules();
-    MyFPGrowtModel model = new MyFPGrowtModel("C:\\Program Files\\Weka-3-8-5\\data\\weather.nominal.arff",
-            "-P 2 -I -1 -N 10 -T 0 -C 0.7 -D 0.05 -U 1.0 -M 0.2",
-            "-N -R first-last");
-    model.mineAssociationRules();
-     System.out.println(model);
+//    MyFPGrowtModel model = new MyFPGrowtModel("C:\\Program Files\\Weka-3-8-5\\data\\weather.nominal.arff",
+//            "-P 2 -I -1 -N 10 -T 0 -C 0.7 -D 0.05 -U 1.0 -M 0.2",
+//            "-N -R first-last");
+//    model.mineAssociationRules();
+//     System.out.println(model);
+//    }
+
+        myknowledgemodel model = new myknowledgemodel( "C:\\Program Files\\Weka-3-8-5\\data\\iris.arff", null, null);
+        model.trainset = model.divideTrainTestR(model.dataset, 80, false);
+        model.testset = model.divideTrainTestR(model.dataset, 80, true);
+        System.out.println(model);
+        System.out.println(model.trainset.toSummaryString());
+        System.out.println(model.testset.toSummaryString());
     }
-    
 }
--- Wekapro/src/wekapro/myknowledgemodel.java
+++ Wekapro/src/wekapro/myknowledgemodel.java
@@ -5,6 +5,7 @@
  */
 package wekapro;
 
+import java.awt.PageAttributes;
 import java.io.File;
 import java.io.IOException;
 import weka.core.Instances;
@@ -15,6 +16,8 @@ import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.NominalToBinary;
 import weka.filters.unsupervised.attribute.NumericToNominal;
 import weka.filters.unsupervised.attribute.Remove;
+import weka.filters.unsupervised.instance.RemovePercentage;
+import weka.filters.unsupervised.instance.Resample;
 
 /**
  *
@@ -25,12 +28,18 @@ public class myknowledgemodel {
     Instances dataset;
     String[] model_options;
     String[] data_optioins;
+    Instances trainset;
+    Instances testset;
 
     public myknowledgemodel(String filename, String m_otps, String d_otps) throws Exception {
         this.source = new DataSource(filename);
         this.dataset = source.getDataSet();
-        this.model_options = weka.core.Utils.splitOptions(m_otps);
+        if(m_otps != null){
+            this.model_options = weka.core.Utils.splitOptions(m_otps);
+        }
+        if(d_otps != null) {
         this.data_optioins = weka.core.Utils.splitOptions(d_otps);
+        }
     }
 
     myknowledgemodel(String cProgram_FilesWeka385datairisarff) {
@@ -75,6 +84,26 @@ public class myknowledgemodel {
         System.out.println("Converted");
     }
     
+    public Instances dividetrainTest(Instances originalSet,
+            double percent, boolean isTest ) throws Exception {
+        RemovePercentage rp = new RemovePercentage();
+        rp.setPercentage(percent);
+        rp.setInvertSelection(isTest);
+        rp.setInputFormat(originalSet);
+        return Filter.useFilter(originalSet, rp);
+        
+        
+    }
+    public Instances divideTrainTestR(Instances originalSet, 
+                            double percent, boolean isTest) throws Exception {
+        Resample rs = new Resample();
+        rs.setNoReplacement(true);
+        rs.setSampleSizePercent(percent);
+        rs.setInvertSelection(isTest);
+        rs.setInputFormat(originalSet);
+        return Filter.useFilter(originalSet, rs);
+    }
+    
 
     @Override
     public String toString() {
@@ -82,6 +111,14 @@ public class myknowledgemodel {
     }
 
     void savedata2CSV(String ddatairis_CSVcsv) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Instances dividetrainTestR(Instances dataset, int i, boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Instances dividetrainTesR(Instances dataset, int i, boolean b) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
