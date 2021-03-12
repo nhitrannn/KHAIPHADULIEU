b0793070550f0432413c0be93471b3d8e714f11b
tuyetnhi220 <trantuyetnhi22032000@gmail.com>
2021-03-12 10:19:26 +0700

    nhi

--- Wekapro/src/wekapro/Wekapro.java
+++ Wekapro/src/wekapro/Wekapro.java
@@ -26,15 +26,15 @@ public class Wekapro {
 //        model.savedata("D:\\data\\iris.arff");
 //        model.saveData2CSV("D:\\data\\iris_CSV.csv");
 
-//     MyAprioriModel model = new MyAprioriModel("C:\\Program Files\\Weka-3-8-5\\data\\weather.numeric.arff",
-//             "-N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1 -S -1.0 -c -1",
-//             "-R 2-3");
-//     model.mineAssociationaRules();
+     MyAprioriModel model = new MyAprioriModel("C:\\Program Files\\Weka-3-8-5\\data\\weather.numeric.arff",
+             "-N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1 -S -1.0 -c -1",
+             "-R 2-3");
+     model.mineAssociationaRules();
 //    MyFPGrowtModel model = new MyFPGrowtModel("C:\\Program Files\\Weka-3-8-5\\data\\weather.nominal.arff",
 //            "-P 2 -I -1 -N 10 -T 0 -C 0.7 -D 0.05 -U 1.0 -M 0.2",
 //            "-N -R first-last");
 //    model.mineAssociationRules();
-//     System.out.println(model);
+     System.out.println(model);
 //    }
 
 //        myknowledgemodel model = new myknowledgemodel( "C:\\Program Files\\Weka-3-8-5\\data\\iris.arff", null, null);
@@ -44,14 +44,14 @@ public class Wekapro {
 //        System.out.println(model.trainset.toSummaryString());
 //        System.out.println(model.testset.toSummaryString());
 
-        MyDecisionTreeModel model = new MyDecisionTreeModel("C:\\Program Files\\Weka-3-8-5\\data\\iris.arff",
-                "-C 0.25 -M 2", null);
-        model.buildDecisionTree();
-        model.evaluateDicisionTree();
-        System.out.println(model);
-       // model.saveModel("D:\\data\\model\\decitiontree.model", model.tree);
-       model.tree = (J48)model.loadModel("D:\\data\\model\\decitiontree.model");
-       model.predictClassLabel(model.testset);
+//        MyDecisionTreeModel model = new MyDecisionTreeModel("C:\\Program Files\\Weka-3-8-5\\data\\iris.arff",
+//                "-C 0.25 -M 2", null);
+//        model.buildDecisionTree();
+//        model.evaluateDicisionTree();
+//        System.out.println(model);
+//       // model.saveModel("D:\\data\\model\\decitiontree.model", model.tree);
+//       model.tree = (J48)model.loadModel("D:\\data\\model\\decitiontree.model");
+//       model.predictClassLabel(model.testset);
         
     }
 }
